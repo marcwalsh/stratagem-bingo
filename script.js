42537b3ff0c10a75b6db041d6e9759aa785a9e91
Marc Walsh <marcwalsh@me.com>
2024-06-15 13:21:42 +0100

    Make it look more like Helldivers

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const stratagems = [
-        "Orbital Laser", "Arc Thrower", "Railgun", "Eagle Airstrike",
-        "Eagle Cluster Bomb", "Auto Cannon", "Quasar Cannon", "Expendable Anti-Tank",
-        "Patriot Exosuit", "Shield Generator Pack", "Grenade Launcher", "EMS Mortar Sentry",
-        "Eagle 500KG Bomb", "Auto Cannon Sentry", "Anti Material Rifle", "Guard Dog Rover",
-        "Railcannon Strike", "Recoilless Rifle", "Stalwart", "Jump Pack",
-        "Laser Cannon", "Orbital Precision Strike", "Shield Generator", "Rocket Sentry",
-        "Flamethrower", "Napalm Airstrike", "Orbital Walking Barrage", "Orbital 380MM HE Barrage",
-        "Orbital Gas Strike", "Orbital EMS Strike", "Orbital Smoke Strike", "HMG Placement",
-        "Shield Generator Relay", "Tesla Tower", "Anti-Personnel Minefield", "Supply Pack"
-    ];
-
-    const shuffleArray = array => {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    };
-
-    shuffleArray(stratagems);
-
-    const bingoGrid = document.getElementById("bingo-grid");
-
-    for (let i = 0; i < 4; i++) {
-        const cell = document.createElement("div");
-        cell.className = "bingo-cell";
-        cell.textContent = stratagems[i];
-        bingoGrid.appendChild(cell);
-    }
-});
-document.addEventListener("DOMContentLoaded", () => {
-    const stratagems = [
         "Orbital_Illumination_Flare", "Hellbomb", "Tectonic_Drill", "Seismic_Probe",
         "SEAF_Artillery", "Reinforce", "Prospecting_Drill", "Super_Earth_Flag",
         "Upload_Data", "Dark_Fluid_Vessel", "Resupply", "SOS_Beacon",
@@ -137,7 +106,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const img = document.createElement("img");
         img.src = stratagemImages[stratagems[i]];
         img.alt = stratagems[i];
+        const text = document.createElement("span");
+        text.textContent = stratagems[i].replace(/_/g, ' ');
         cell.appendChild(img);
+        cell.appendChild(text);
         bingoGrid.appendChild(cell);
     }
 });
@@ -175,7 +147,7 @@ function printBingoCard() {
     `);
 
     stratagems.forEach((stratagem, index) => {
-        printWindow.document.write(`<div class="bingo-cell"><img src="${stratagemImages[index]}" alt="${stratagem}"></div>`);
+        printWindow.document.write(`<div class="bingo-cell"><img src="${stratagemImages[index]}" alt="${stratagem}"><span>${stratagem.replace(/_/g, ' ')}</span></div>`);
     });
 
     printWindow.document.write(`
@@ -220,7 +192,7 @@ function printMiniBingoCard() {
     `);
 
     stratagems.forEach((stratagem, index) => {
-        printWindow.document.write(`<div class="bingo-cell"><img src="${stratagemImages[index]}" alt="${stratagem}"></div>`);
+        printWindow.document.write(`<div class="bingo-cell"><img src="${stratagemImages[index]}" alt="${stratagem}"><span>${stratagem.replace(/_/g, ' ')}</span></div>`);
     });
 
     printWindow.document.write(`
